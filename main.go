1c6aacdf31146af00840f37e00b904042c82f3e6
Marcus <macansorm@hotmail.com>
2025-06-04 14:37:02 +0200

    Fix main.go to serve React page

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 )
 
+func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
 	fmt.Println("Listening on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Server error:", err)
